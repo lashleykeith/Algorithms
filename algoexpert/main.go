29c7c244f93dacded093e55dd8266e3a442570dc
Keith Lashley <lashleykeith@gmail.com>
2023-03-29 18:55:44 +0900

    Add files via upload

--- algoexpert/main.go
+++ algoexpert/main.go
@@ -1,5 +1,3 @@
-// O(nlogn) time | O(1) space
-
 package main
 
 import (
@@ -7,26 +5,54 @@ import (
 )
 
 
-func MinimumWaitingTime(queries []int) int{
-   sort.Ints(queries)
+func TandemBicycle(redShirtSpeeds []int, blueShirtSpeeds []int, fastest bool) int{
+    sort.Ints(redShirtSpeeds)
+    sort.Ints(blueShirtSpeeds)
 
-   totalWaitingTime := 0
-   for idx, duration := range queries {
-    queriesLeft := len(queries) - (idx + 1)
-    totalWaitingTime += duration * queriesLeft
-   }
+    if !fastest{
+        reverseArrayInPlace(redShirtSpeeds)
+    }
 
-   return totalWaitingTime
+    totalSpeed := 0
+    for idx := range redShirtSpeeds{
+        rider1 := redShirtSpeeds[idx]
+        rider2 := blueShirtSpeeds[len(blueShirtSpeeds)-idx-1]
+        totalSpeed += max(rider1, rider2)
+    }
+    return totalSpeed
+
+}
+
+func reverseArrayInPlace(array []int){
+    var start = 0
+    var end = len(array) - 1
+    for start < end {
+        temp := array[start]
+        array[start] = array[end]
+        array[end] = temp
+        start += 1
+        end -= 1
+    }
+}
+
+
+func max(a, b int) int{
+    if a > b {
+        return a
+    }
+    return b
 }
 
 /*
-We need to edit this code to print out the the totalWaitingTime of this query.
+create a function TandemBicycle holding in the parameters redShirtSpeeds,  blueShirtSpeeds and a fastest for the bool
 
-This is the query
-{
-  "queries": [3, 2, 1, 2, 6]
+sort the red and blue ShirtSpeeds
 
-}
+if not the fastest we need to reverseArrayInPlace
 
-This is the code:
+create the reverseArrayInPace function it hols an array
+  intialize start
+  intialize end to be at the end of the array
+
+  for loop as long as start is less than end
 */
